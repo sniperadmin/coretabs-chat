751fae0abe0726e426183a819ba50731c7ea32d8
Nasr Galal <nasrbue@gmail.com>
2019-12-19 13:57:38 +0200

    created front design

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,143 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <!-- code here -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row justify-content-center">
+            <h4 class="text-md-center">Responsive Chat Template</h4>
+          </div>
+          <div class="row">
+            <div class="col">
+              <ul>
+                <li style="list-style: none;">
+                  <div class="card card-info pb-chat-panel">
+                    <div class="card card-header pb-chat-panel-heading">
+                      <div class="row">
+                        <div class="col-xs-12">
+                          <label id="support_label">Lorem support</label>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-block">
+                      <form style="border: 1px solid grey">
+                        <div class="form-group" style="text-align: left;">
+                          <span class="fa fa-lg fa-user pb-chat-fa-user"></span>
+                          <span
+                            class="tag label-default pb-chat-labels pb-chat-labels-left"
+                          >
+                            Hi, how are you?
+                          </span>
+                        </div>
+                        <hr />
+                        <div
+                          class="form-group float-xs-right pb-chat-labels-right"
+                        >
+                          <span
+                            class="tag label-primary pb-chat-labels pb-chat-labels-primary"
+                            >Hi, i'm fine, you?</span
+                          ><span
+                            class="fa fa-lg fa-user pb-chat-fa-user"
+                          ></span>
+                        </div>
+                        <div class="clearfix"></div>
+                        <hr />
+                      </form>
+                    </div>
+                    <div class="card-footer">
+                      <div class="row">
+                        <div class="col-md-11">
+                          <textarea
+                            class="form-control pb-chat-textarea"
+                            placeholder="Type your message here..."
+                          ></textarea>
+                        </div>
+                        <div class="col-xs-2 pb-btn-circle-div">
+                          <button
+                            class="btn btn-primary btn-circle pb-chat-btn-circle"
+                          >
+                            <span class="fa fa-chevron-right"></span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  name: "HelloWorld"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style scoped>
+.pb-chat-panel {
+  border: none;
+  margin-bottom: -5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.pb-chat-panel-heading {
+  margin-top: -5px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.pb-chat-top-icons {
+  margin-top: 4px;
 }
-a {
-  color: #42b983;
+
+#support_label {
+  color: #999;
+}
+
+.pb-chat-labels {
+  font-size: 15px;
+}
+
+.pb-chat-labels-right {
+  margin-bottom: 0;
+  margin-right: 5px;
+  text-align: right;
+}
+
+.pb-chat-labels-left {
+  margin-bottom: 0;
+  margin-left: 10px;
+}
+
+.pb-chat-labels-primary {
+  margin-right: 5px;
+}
+
+.pb-chat-fa-user {
+  border: 1px solid blue;
+  padding: 5px;
+  border-radius: 50%;
+  margin: 5px;
+}
+
+.pb-chat-textarea {
+  /* resize: none; */
+  width: 100%;
+}
+
+.pb-chat-dropdown {
+  width: 300px;
+}
+
+.pb-chat-btn-circle {
+  border-radius: 35px;
+}
+
+.pb-btn-circle-div {
+  padding-left: 0px;
+  margin-top: 12px;
 }
 </style>
